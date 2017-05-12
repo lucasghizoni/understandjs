658e5cbbf82eb2ee38d0d98c3e06db24e91bf212
lucasghizoni <lucasghizoni@gmail.com>
2017-05-12 12:58:53 -0300

    added not defined and undefined example

---
+++ notdefined-and-undefined.js
@@ -0,0 +1,3 @@
+console.log(b); // this will print undefined, because you declared, not matter where is declared, before ow after this line.
+var b;
+console.log(a); //this will give you an error: "a is not defined", because u didnt declare
