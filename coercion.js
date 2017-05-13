9ff848995ee2b24c82a2c3d4a180396d3237d38c
Lucas Ghizoni <lucasghizoni@gmail.com>
2017-05-13 19:07:54 -0300

    added more examples to coercion with comparator equal

--- coercion.js
+++ coercion.js
@@ -1,2 +1,5 @@
 var a = 1 + '2';
 console.log(a); // will print 12. the engine coerce the number into string when summs with another string.
+
+console.log(2 == "2"); // will print true because of coercion. 
+console.log(2 === "2"); // will print false because the engine doesnt try to coerce, when its triple equals.
