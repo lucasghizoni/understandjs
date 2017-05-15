bd666218cbefc44a8cd65fa04d71b68363acd5c2
Lucas Ghizoni <lucasghizoni@gmail.com>
2017-05-15 00:01:08 -0300

    Objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+var myObj = new Object();
+
+myObj['firstName'] = "Lucas";
+
+
+var anotherObj = { //creating object literally. Its the same as the line one.
+	name: "Lucas",
+	addres: "my Address"
+};
