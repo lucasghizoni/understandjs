ead46cd087db03733adf4eddd564548fa28e7ec2
Lucas Ghizoni <lucasghizoni@gmail.com>
2017-05-23 22:44:02 -0300

    examples

---
+++ immediately-invokable-funcs-expressions.js
@@ -0,0 +1,9 @@
+function(){
+	console.log("Teste!");
+} // this will print an error with the engine of JS.
+
+//if you want to create a function on the fly, just add "(  )" around the function, like an expression that contains a function
+
+(function(){
+	console.log("Teste!");
+})
